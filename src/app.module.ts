265618b80e5f3d53b7ff29efdee1e4c8d93ad40c
rishabh2671 <rishabh2671@gmail.com>
2023-01-11 23:11:24 +0530

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NewModule } from './new/new.module';
 
 @Module({
-  imports: [],
+  imports: [NewModule],
   controllers: [AppController],
   providers: [AppService],
 })
